0f1a30f76e8c10845b4e6b22bcd1772005d430ba
Aditya Prasad <adityak2541@gmail.com>
2025-06-02 21:54:10 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  upi: { type: String, required: true },
+  upi: { type: String,},
   paytmNumber: { type: String, required: true },
   campaignName: { type: String, required: true }, // Store which campaign user participated in
   createdAt: { type: Date, default: Date.now },
