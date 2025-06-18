f0a4f3674d5d81213200654c970b8f8b8d6f4137
Aditya Prasad <adityak2541@gmail.com>
2025-06-18 15:24:06 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  upi: { type: String,},
+  upi: { type: String, },
   paytmNumber: { type: String, required: true },
   campaignName: { type: String, required: true }, // Store which campaign user participated in
   createdAt: { type: Date, default: Date.now },
 });
+
+UserSchema.index({ paytmNumber: 1, campaignName: 1 }, { unique: true });
+UserSchema.index({ upi: 1, campaignName: 1 }, { unique: true, sparse: true });
+
 
 module.exports = mongoose.model("User", UserSchema);
